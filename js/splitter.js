829614340a26d403b2fcfeddae2208b42526b620
Maxim Titov <maxim.titov@arcadia.spb.ru>
2016-11-11 21:05:19 +0300

    Horizontal split handler handles scroll event. Outside styling. Refactoring.

--- js/splitter.js
+++ js/splitter.js
@@ -1,91 +1,149 @@
 angular.module('bgDirectives', [])
-  .directive('bgSplitter', function() {
-    return {
-      restrict: 'E',
-      replace: true,
-      transclude: true,
-      scope: {
-        orientation: '@'
-      },      
-      template: '<div class="split-panes {{orientation}}" ng-transclude></div>',
-      controller: ['$scope', function ($scope) {
-        $scope.panes = [];
-        
-        this.addPane = function(pane){
-          if ($scope.panes.length > 1) 
-            throw 'splitters can only have two panes';
-          $scope.panes.push(pane);
-          return $scope.panes.length;
-        };
-      }],
-      link: function(scope, element, attrs) {
-        var handler = angular.element('<div class="split-handler"></div>');
-        var pane1 = scope.panes[0];
-        var pane2 = scope.panes[1];
-        var vertical = scope.orientation == 'vertical';
-        var pane1Min = pane1.minSize || 0;
-        var pane2Min = pane2.minSize || 0;
-        var drag = false;
-        
-        pane1.elem.after(handler);
-        
-        element.bind('mousemove', function (ev) {
-          if (!drag) return;
-          
-          var bounds = element[0].getBoundingClientRect();
-          var pos = 0;
-          
-          if (vertical) {
+  .directive('bgScroll', Scroll)
+  .directive('bgSplitter', Splitter)
+  .directive('bgPane', Pane);
 
-            var height = bounds.bottom - bounds.top;
-            pos = ev.clientY - bounds.top;
+Splitter.$inject = ['$document'];
 
-            if (pos < pane1Min) return;
-            if (height - pos < pane2Min) return;
+function Scroll () {
+  return {
+    restrict: 'A',
+    controller: function () {
+      this.handler = null;
+    },
+    link: function(scope, element, attrs, ctrl) {
+      element.on('scroll', onscroll);
 
-            handler.css('top', pos + 'px');
-            pane1.elem.css('height', pos + 'px');
-            pane2.elem.css('top', pos + 'px');
-      
-          } else {
+      scope.$on('$destroy', function () {
+        element.off('scroll', onscroll);
+      });
 
-            var width = bounds.right - bounds.left;
-            pos = ev.clientX - bounds.left;
-
-            if (pos < pane1Min) return;
-            if (width - pos < pane2Min) return;
-
-            handler.css('left', pos + 'px');
-            pane1.elem.css('width', pos + 'px');
-            pane2.elem.css('left', pos + 'px');
-          }
-          scope.$apply();
-        });
-    
-        handler.bind('mousedown', function (ev) { 
-          ev.preventDefault();
-          drag = true; 
-        });
-    
-        angular.element(document).bind('mouseup', function (ev) {
-          drag = false;
-        });
+      function onscroll (ev) {
+        var scrollTop = element.prop('scrollTop'),
+          containerTop = element.prop('offsetTop'),
+          height = element.prop('clientHeight'),
+          top = scrollTop - containerTop + height / 2;
+        ctrl.handler.css('top', top + 'px');
       }
-    };
-  })
-  .directive('bgPane', function () {
-    return {
-      restrict: 'E',
-      require: '^bgSplitter',
-      replace: true,
-      transclude: true,
-      scope: {
-        minSize: '='
-      },
-      template: '<div class="split-pane{{index}}" ng-transclude></div>',
-      link: function(scope, element, attrs, bgSplitterCtrl) {
-        scope.elem = element;
-        scope.index = bgSplitterCtrl.addPane(scope);
+    }
+  }
+}
+
+function Splitter ($document) {
+  return {
+    restrict: 'E',
+    replace: true,
+    require: '^?bgScroll',
+    transclude: true,
+    scope: {
+      orientation: '@',
+      handlerStyle: '='
+    },
+    template: '<div class="split-panes {{orientation}}" ng-transclude></div>',
+    controller: ['$scope', function ($scope) {
+      $scope.panes = [];
+
+      this.addPane = function(pane){
+        if ($scope.panes.length > 1)
+          throw 'splitters can only have two panes';
+        $scope.panes.push(pane);
+        return $scope.panes.length;
+      };
+    }],
+    link: function(scope, element, attrs, ctrl) {
+      var handler = angular.element('<div class="split-handler"></div>'),
+        pane1 = scope.panes[0],
+        pane2 = scope.panes[1],
+        vertical = scope.orientation === 'vertical',
+        pane1Min = pane1.minSize || 0,
+        pane2Min = pane2.minSize || 0,
+        draged = false,
+        handlerStyle = angular.extend({
+          width: '10px',
+          height: '10px'
+        }, scope.handlerStyle);
+
+      if (vertical) {
+        handlerStyle['left'] = '50%';
+      } else {
+        handlerStyle['top'] = '50%';
       }
-    };
-  });
+      handler.css(handlerStyle);
+
+      if (ctrl) {
+        ctrl.handler = handler;
+      }
+
+      pane1.elem.after(handler);
+
+      element.bind('mousemove', drag);
+      handler.bind('mousedown', dragstart);
+      $document.bind('mouseup', dragend);
+
+      scope.$on('$destroy', function () {
+        element.unbind('mousemove', drag);
+        handler.unbind('mousedown', dragstart);
+        $document.unbind('mouseup', dragend);
+      });
+
+      function drag (ev) {
+        if (!draged) return;
+
+        var bounds = element[0].getBoundingClientRect();
+        var pos = 0;
+
+        if (vertical) {
+
+          var height = bounds.bottom - bounds.top;
+          pos = ev.clientY - bounds.top;
+
+          if (pos < pane1Min) return;
+          if (height - pos < pane2Min) return;
+
+          handler.css('top', pos + 'px');
+          pane1.elem.css('height', pos + 'px');
+          pane2.elem.css('top', pos + 'px');
+
+        } else {
+
+          var width = bounds.right - bounds.left;
+          pos = ev.clientX - bounds.left;
+
+          if (pos < pane1Min) return;
+          if (width - pos < pane2Min) return;
+
+          handler.css('left', pos + 'px');
+          pane1.elem.css('width', pos + 'px');
+          pane2.elem.css('left', pos + 'px');
+        }
+        scope.$apply();
+      }
+
+      function dragstart (ev) {
+        ev.preventDefault();
+        draged = true;
+      }
+
+      function dragend (ev) {
+        draged = false;
+      }
+    }
+  };
+}
+
+function Pane () {
+  return {
+    restrict: 'E',
+    require: '^bgSplitter',
+    replace: true,
+    transclude: true,
+    scope: {
+      minSize: '='
+    },
+    template: '<div class="split-pane{{index}}" ng-transclude></div>',
+    link: function(scope, element, attrs, bgSplitterCtrl) {
+      scope.elem = element;
+      scope.index = bgSplitterCtrl.addPane(scope);
+    }
+  };
+}
